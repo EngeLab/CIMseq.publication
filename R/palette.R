e7a0d7fe02b8cbb306a4d641187c2072e70200c5
Jason Serviss <jason.serviss@ki.se>
2019-08-21 11:30:51 +0200

    bug fix in palette

--- R/palette.R
+++ R/palette.R
@@ -39,19 +39,19 @@ palette <- function(type) {
 .palette.si <- function() {
   c(
     Tufft = "#ec4339", Enteroendocrine = "#828585", Blood = "#9B9700", 
-    `SI Goblet` = "#DDEFFF", `SI Paneth` = "#7B4F4B", `SI Stem` = "#A1C299", 
-    `SI Transit amplifying` = "#0AA6D8", `SI Progenitor early` = "#00846F", 
-    `SI Progenitor late` = "#FFB500", `SI Enterocytes` = "#C2FFED"
+    `Goblet` = "#DDEFFF", `Paneth` = "#7B4F4B", `Stem` = "#A1C299", 
+    `Transit amplifying` = "#0AA6D8", `Progenitor early` = "#00846F", 
+    `Progenitor late` = "#FFB500", `Enterocytes` = "#C2FFED"
   )
 }
 
 .palette.c <- function() {
   c(
-    `C Stem 1` = "#C2FF99", `C Stem 2` = "#C0B9B2", 
-    `C Stem 3` = "#FF8A9A", `C Transit amplifying` = "#6F0062", 
-    `C Progenitor` = "#FECFAD",  `C Colonocytes` = "#00489C", 
-    `C Goblet proliferating` = "#EEC3FF", `C Goblet Plet1 1` = "#e0a81c", 
-    `C Goblet Plet1 2` = "#922329", `C Goblet Plet1-` = "#FFF69F", 
+    `Stem 1` = "#C2FF99", `Stem 2` = "#C0B9B2", 
+    `Stem 3` = "#FF8A9A", `Transit amplifying` = "#6F0062", 
+    `Progenitor` = "#FECFAD",  `Colonocytes` = "#00489C", 
+    `Goblet proliferating` = "#EEC3FF", `Goblet Plet1 1` = "#e0a81c", 
+    `Goblet Plet1 2` = "#922329", `Goblet Plet1-` = "#FFF69F", 
     Tufft = "#ec4339", Enteroendocrine = "#828585", Blood = "#9B9700"
   )
 }
